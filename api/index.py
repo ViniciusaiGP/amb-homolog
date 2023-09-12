36b4708486b3b90de3a9232e3e734a0383c94abc
Vinícius Mota Zulianelli <70486496+ViniciusaiGP@users.noreply.github.com>
2023-09-12 08:55:46 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("amb-homolog-jwt-firebase-adminsdk-ijm5w-698873dea5.json")
+cred = credentials.Certificate("api\amb-homolog-jwt-firebase-adminsdk-ijm5w-698873dea5.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()  # Inicializa o Firestore
 
