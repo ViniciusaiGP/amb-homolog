8e855bcb5ea2f723bfb606876f920c575e2c3bd4
Vinícius Mota Zulianelli <70486496+ViniciusaiGP@users.noreply.github.com>
2023-09-12 07:59:35 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,5 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-import json
-from flask_cors import CORS
-import os
+
 
 app = Flask(__name__)
 
