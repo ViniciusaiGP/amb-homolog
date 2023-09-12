04783f0b91e42b0cc5cbcf86988d64be3e727d7a
Vinícius Mota Zulianelli <70486496+ViniciusaiGP@users.noreply.github.com>
2023-09-12 08:11:37 -0300

    update config JWT

--- api/index.py
+++ api/index.py
@@ -1,8 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_jwt_extended import *
 from flask_cors import CORS
+import json
+import os
 
 app = Flask(__name__)
+
+# Configuração do Flask-JWT-Extended
+app.config['JWT_SECRET_KEY'] = 'secreto'  # Troque isso por uma chave secreta mais segura em um ambiente de produção
+jwt = JWTManager(app)
+
+# Simulação de um banco de dados de usuários
+users = {
+    'admin': '1234'
+}
+
+# Verifica se o arquivo JSON existe e, se não, cria uma lista vazia
+if not os.path.exists('agent_data.json'):
+    with open('agent_data.json', 'w') as file:
+        json.dump([], file)
 
 @app.route('/')
 def home():
